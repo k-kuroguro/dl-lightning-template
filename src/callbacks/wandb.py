a82b00298372ba43e75d7a5a52898d7c6025a118
k-kuroguro <lingjiujinshan@gmail.com>
2025-01-27 00:42:47 +0900

    feat: Add callback for watching model with W&B

---
+++ src/callbacks/wandb.py
@@ -0,0 +1,43 @@
+from typing import Literal
+
+from lightning.pytorch import Callback, LightningModule, Trainer
+from lightning.pytorch.loggers import WandbLogger
+from lightning_utilities.core.rank_zero import rank_zero_only
+
+
+def _get_wandb_logger(trainer: Trainer) -> WandbLogger:
+    """Get the W&B logger from the trainer.
+
+    Raises:
+        ValueError: If the W&B logger is not found.
+    """
+    for logger in trainer.loggers:
+        if isinstance(logger, WandbLogger):
+            return logger
+
+    raise ValueError("WandbLogger not found in spite of using wandb related callbacks.")
+
+
+class ModelWatcher(Callback):
+    """Watch the model with W&B at the start of training."""
+
+    def __init__(
+        self,
+        *,
+        log: Literal["gradients", "parameters", "all"] = "gradients",
+        log_freq: int = 1000,
+    ) -> None:
+        self._log = log
+        self._log_freq = log_freq
+
+    @rank_zero_only
+    def on_train_start(self, trainer: Trainer, pl_module: LightningModule) -> None:
+        assert trainer.model is not None
+
+        logger = _get_wandb_logger(trainer=trainer)
+        logger.watch(
+            model=trainer.model,
+            log=self._log,
+            log_freq=self._log_freq,
+            log_graph=False,  # Setting this to False to avoid errors when used with ModelCheckpoint. cf. https://github.com/wandb/wandb/issues/2588
+        )
